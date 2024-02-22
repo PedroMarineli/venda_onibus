bd60d998ee954dae0105b8187c9cbf7afb8499df
PedroMarineli <pedrohenrique.marineli@gmail.com>
2024-02-22 19:54:04 +0000

    criação da funcão escolherOpcoes e criação das structs.

--- main.c
+++ main.c
@@ -3,6 +3,31 @@
 
 void mostraOpcoes();
 void mostraTitulo(char*);
+void escolherOpcoes();
+
+struct 
+{
+    int id;
+    int janela;
+    int andar;
+} cadastroAssento[50];
+
+struct
+{
+    int quantidadeDeAssentos;
+} cadastroOnibus[2];
+
+struct
+{
+    char nome[40];
+} cadadastroCidade[5];
+
+struct
+{
+    int preco;
+
+    struct cadadastroCidade cidade;
+} cadastroPassagem;
 
 int main(){
     mostraTitulo("VENDA DE PASSAGENS DE ÔNIBUS");
@@ -10,17 +35,38 @@ int main(){
 }
 
 void mostraOpcoes(){
-    printf("Escolha uma opção:\n");
+    printf("Escolha uma opção:\n\n");
     printf("1. Comprar passagens\n");
     printf("2. Verificar disponibilidade de assentos\n");
     printf("3. Sair\n");
+    
+    escolherOpcoes();
 }
 
 void mostraTitulo(char titulo[]){
     int i;
     int tamanho_titulo = (strlen(titulo) + 4);
+
     for (i=1; i < tamanho_titulo; i++) printf("─");
     printf("\n  %s\n", titulo);
     for (i=1; i < tamanho_titulo; i++) printf("─");
     printf("\n");
+}
+
+void escolherOpcoes(){
+    int opcao;
+
+    printf("\nOpção escolhida: ");
+    scanf("%d", &opcao);
+
+    switch (opcao) {
+        case 1:
+            break;
+        case 2:
+            break;
+        case 3:
+            break;
+        default:
+            printf("Opção inválida\n");
+    }
 }
